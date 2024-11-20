75ec2028d7236f1df7ba39f5aca2344da93ec306
SCHcoding <codingb007@gmail.com>
2024-11-20 16:52:18 +0900

    Test: Pie, Radar Chart Test

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,91 +1,174 @@
 "use client"
 
-import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend } from 'chart.js';
-import { useRef } from 'react';
-import { getElementsAtEvent, Line } from 'react-chartjs-2';
+import { Chart as ChartJS, RadialLinearScale, LineElement, PointElement, Filler, Title, Tooltip, Legend } from 'chart.js';
+import { Radar } from 'react-chartjs-2';
 
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+ChartJS.register(RadialLinearScale, LineElement, PointElement, Filler, Title, Tooltip, Legend);
+
 
 const MyChart = () => {
     const data = {
         labels: ['Mon', 'Tue', 'Wed'],
-        datasets: [
-            {
+        datasets: [{
                 label: 'Weekdays',
-                // 데이터는 배열값으로 받아야함 
                 data: [33, 53, 85],
-                fill: false,
-                borderColor: 'green',
-                // backgroundColor는 밑에 ㅁ 네모 칸 색깔을 채우는 속성
-                backgroundColor: 'green',
-                // 곡선 정도를 설정하는 속성. 이 속성은 선의 부드러움을 조정하는 데 사용
-                // 0에 가까울수록 직선에 가까워짐
-                tension: 0.4,   
-                link: ['https://www.daum.net', 'https://www.naver.com', 'https://www.google.com'],
-            },
-            {
-                label: 'Years',
-                // 데이터는 배열값으로 받아야함 
-                data: [13, 95, 22],
-                fill: false,
-                borderColor: 'blue',
-                backgroundColor: '',
-                // 곡선 정도를 설정하는 속성. 이 속성은 선의 부드러움을 조정하는 데 사용
-                // 0에 가까울수록 직선에 가까워짐
-                tension: 0.4,   
-                link: ['https://www.daum.net', 'https://www.naver.com', 'https://www.google.com'],
-            },
-        ],
-    };
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'bottom',
-                labels: {
-                    font: {
-                        size: 20,
-                    },
-                },
-            },
-            title: {
-                display: true,
-                text: 'Chart Test',
-                font: {
-                    size: 35,
-                },
-            },
-        },
-    };
-
-    const chartRef = useRef();
-    const onClickTest = (e) => {
-        if(getElementsAtEvent(chartRef.current, e).length > 0) {
-            console.log(getElementsAtEvent(chartRef.current, e))
-
-            const clickDatasetIndex = getElementsAtEvent(chartRef.current, e)[0].datasetIndex;
-            console.log("clickDatasetIndex = ", clickDatasetIndex)
-
-            const dataPoint = getElementsAtEvent(chartRef.current, e)[0].index;
-            console.log("dataPoint = ", dataPoint)
-            console.log("Data = ", data.datasets[clickDatasetIndex].link[dataPoint])
-            
-            const link = data.datasets[clickDatasetIndex].link[dataPoint]
-            window.open(link, '_blank')
-
-        }
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'black',
+            }]
     }
 
-    return (
-    <>
-    <Line data={data} options={options} onClick={onClickTest} ref={chartRef} />
-    </>
-    );
+    const options = {
+    }
 
+    
+    return (
+        <div className='w-2/3'>
+            <Radar data={data} options={options} />
+        </div>
+    )
 }
 
 export default MyChart;
+
+// import { Chart as ChartJS, CategoryScale, LinearScale, ArcElement, Title, Tooltip, Legend } from 'chart.js';
+// import { Pie } from 'react-chartjs-2';
+
+// ChartJS.register(CategoryScale, LinearScale, ArcElement, Title, Tooltip, Legend);
+
+// const MyChart = () => {
+//     const data = {
+//         labels: ['Red', 'Blue', 'Yellow'],
+//         datasets: [
+//             {
+//                 label: 'Pie Chart Test',
+//                 data: [3, 6, 9],
+//                 backgroundColor: [
+//                     'rgb(255, 99, 132)',
+//                     'rgb(54, 162, 235)',
+//                     'rgb(255, 205, 86)',
+//                 ],
+//                 borderColor: 'black',   
+//                 hoverOffset: 4,
+//             },
+//         ],
+//     };
+
+//     const options = {
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 position: 'bottom',
+//                 labels: {
+//                     font: {
+//                         size: 20,
+//                     },
+//                 },
+//             },
+//             title: {
+//                 display: true,
+//                 text: 'Chart Test',
+//                 font: {
+//                     size: 35,
+//                 },
+//             },
+//         },
+//     };
+
+//     return (
+//         <div className='w-1/3'>
+//         <Pie data={data} options={options} />
+//         </div>
+//     )
+// }
+
+// export default MyChart;
+
+// import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend } from 'chart.js';
+// import { useRef } from 'react';
+// import { getElementsAtEvent, Line } from 'react-chartjs-2';
+
+// ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+
+// const MyChart = () => {
+//     const data = {
+//         labels: ['Mon', 'Tue', 'Wed'],
+//         datasets: [
+//             {
+//                 label: 'Weekdays',
+//                 // 데이터는 배열값으로 받아야함 
+//                 data: [33, 53, 85],
+//                 fill: false,
+//                 borderColor: 'green',
+//                 // backgroundColor는 밑에 ㅁ 네모 칸 색깔을 채우는 속성
+//                 backgroundColor: 'green',
+//                 // 곡선 정도를 설정하는 속성. 이 속성은 선의 부드러움을 조정하는 데 사용
+//                 // 0에 가까울수록 직선에 가까워짐
+//                 tension: 0.4,   
+//                 link: ['https://www.daum.net', 'https://www.naver.com', 'https://www.google.com'],
+//             },
+//             {
+//                 label: 'Years',
+//                 // 데이터는 배열값으로 받아야함 
+//                 data: [13, 95, 22],
+//                 fill: false,
+//                 borderColor: 'blue',
+//                 backgroundColor: '',
+//                 // 곡선 정도를 설정하는 속성. 이 속성은 선의 부드러움을 조정하는 데 사용
+//                 // 0에 가까울수록 직선에 가까워짐
+//                 tension: 0.4,   
+//                 link: ['https://www.daum.net', 'https://www.naver.com', 'https://www.google.com'],
+//             },
+//         ],
+//     };
+
+//     const options = {
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 position: 'bottom',
+//                 labels: {
+//                     font: {
+//                         size: 20,
+//                     },
+//                 },
+//             },
+//             title: {
+//                 display: true,
+//                 text: 'Chart Test',
+//                 font: {
+//                     size: 35,
+//                 },
+//             },
+//         },
+//     };
+
+//     const chartRef = useRef();
+//     const onClickTest = (e) => {
+//         if(getElementsAtEvent(chartRef.current, e).length > 0) {
+//             console.log(getElementsAtEvent(chartRef.current, e))
+
+//             const clickDatasetIndex = getElementsAtEvent(chartRef.current, e)[0].datasetIndex;
+//             console.log("clickDatasetIndex = ", clickDatasetIndex)
+
+//             const dataPoint = getElementsAtEvent(chartRef.current, e)[0].index;
+//             console.log("dataPoint = ", dataPoint)
+//             console.log("Data = ", data.datasets[clickDatasetIndex].link[dataPoint])
+
+//             const link = data.datasets[clickDatasetIndex].link[dataPoint]
+//             window.open(link, '_blank')
+
+//         }
+//     }
+
+//     return (
+//     <>
+//     <Line data={data} options={options} onClick={onClickTest} ref={chartRef} />
+//     </>
+//     );
+
+// }
+
+// export default MyChart;
 
 
 
