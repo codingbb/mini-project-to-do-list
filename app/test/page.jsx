2ecee4ffa1616777948847db3e31dab6b8389b27
SCHcoding <codingb007@gmail.com>
2024-11-21 16:53:05 +0900

    Test: BarChart 그라데이션

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,89 +1,89 @@
 "use client"
 
-import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+// import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js';
+// import { Doughnut } from 'react-chartjs-2';
 
-ChartJS.register(ArcElement, Title, Tooltip, Legend);
+// ChartJS.register(ArcElement, Title, Tooltip, Legend);
 
-const MyChart = () => {
-    const data = {
-        labels: ['Yes', 'No'],
-        datasets: [{
-                label: 'Poll',
-                data: [13, 6],
-                backgroundColor: (context) => {
-                    console.log(context);
-                    const chart = context.chart;
-                    const { ctx, chartArea } = chart;
-                    console.log("chartArea = ", chartArea);
-                    // 애니메이션 촤라락 
-                    if (!chartArea) {
-                        return null;
-                    }
+// const MyChart = () => {
+//     const data = {
+//         labels: ['Yes', 'No'],
+//         datasets: [{
+//                 label: 'Poll',
+//                 data: [13, 6],
+//                 backgroundColor: (context) => {
+//                     console.log(context);
+//                     const chart = context.chart;
+//                     const { ctx, chartArea } = chart;
+//                     console.log("chartArea = ", chartArea);
+//                     // 애니메이션 촤라락 
+//                     if (!chartArea) {
+//                         return null;
+//                     }
 
-                    if(context.dataIndex === 0) {
-                        return getGradient(chart);
-                    } else {
-                        return 'blue';
-                    }
-                },
-                borderColor: ['purple', 'blue'],
-                circumference: 180,
-                rotation: 270,
-            }]
-    }
+//                     if(context.dataIndex === 0) {
+//                         return getGradient(chart);
+//                     } else {
+//                         return 'blue';
+//                     }
+//                 },
+//                 borderColor: ['purple', 'blue'],
+//                 circumference: 180,
+//                 rotation: 270,
+//             }]
+//     }
 
-    // 그라데이션 색깔 줌 
-    function getGradient(chart) {
-        const {ctx, chartArea: {top, bottom, left, right}} = chart;
-        const gradientSegment = ctx.createLinearGradient(left, 0, right, 0);
-        gradientSegment.addColorStop(0, 'red');
-        gradientSegment.addColorStop(0.5, 'orange');
-        gradientSegment.addColorStop(1, 'green');
-        return gradientSegment;
+//     // 그라데이션 색깔 줌 
+//     function getGradient(chart) {
+//         const {ctx, chartArea: {top, bottom, left, right}} = chart;
+//         const gradientSegment = ctx.createLinearGradient(left, 0, right, 0);
+//         gradientSegment.addColorStop(0, 'red');
+//         gradientSegment.addColorStop(0.5, 'orange');
+//         gradientSegment.addColorStop(1, 'green');
+//         return gradientSegment;
 
-    }
+//     }
 
-    const options = {
-    }
+//     const options = {
+//     }
 
-    // const gaugeBackground = {
-    //     id: `gaugeBackground`,
-    //     beforeDatasetsDraw(chart, args, pluginOptions) {
-    //         const { ctx, chartArea: {top, bottom, left, right, width, height} } = chart;
-    //         ctx.save();
-    //         ctx.fillStyle = 'gray';
-    //         ctx.fillRect(left, top, width, height);
-    //     }
-    // }
+//     // const gaugeBackground = {
+//     //     id: `gaugeBackground`,
+//     //     beforeDatasetsDraw(chart, args, pluginOptions) {
+//     //         const { ctx, chartArea: {top, bottom, left, right, width, height} } = chart;
+//     //         ctx.save();
+//     //         ctx.fillStyle = 'gray';
+//     //         ctx.fillRect(left, top, width, height);
+//     //     }
+//     // }
 
-    const textCenter = {
-        id: `textCenter`,
-        beforeDatasetsDraw(chart, args, pluginOptions) {
-            const { ctx, data } = chart;
+//     const textCenter = {
+//         id: `textCenter`,
+//         beforeDatasetsDraw(chart, args, pluginOptions) {
+//             const { ctx, data } = chart;
 
-            // text 쓸 중심값 구하기
-            const xCenter = chart.getDatasetMeta(0).data[0].x;
-            const yCenter = chart.getDatasetMeta(0).data[0].y;
+//             // text 쓸 중심값 구하기
+//             const xCenter = chart.getDatasetMeta(0).data[0].x;
+//             const yCenter = chart.getDatasetMeta(0).data[0].y;
 
-            ctx.save();
-            ctx.font = 'bolder 30px sans-serif';
-            ctx.fillStyle = 'red';
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillText(`Value : ${data.datasets[0].data[0]}`, xCenter, yCenter);
+//             ctx.save();
+//             ctx.font = 'bolder 30px sans-serif';
+//             ctx.fillStyle = 'red';
+//             ctx.textAlign = 'center';
+//             ctx.textBaseline = 'middle';
+//             ctx.fillText(`Value : ${data.datasets[0].data[0]}`, xCenter, yCenter);
 
-        }
-    }
+//         }
+//     }
 
-    return (
-        <div className='w-1/3'>
-            <Doughnut data={data} options={options} plugins={[textCenter]} />
-        </div>
-    )
-}
+//     return (
+//         <div className='w-1/3'>
+//             <Doughnut data={data} options={options} plugins={[textCenter]} />
+//         </div>
+//     )
+// }
 
-export default MyChart;
+// export default MyChart;
 
 // import { Chart as ChartJS, RadialLinearScale, LineElement, PointElement, Filler, Title, Tooltip, Legend } from 'chart.js';
 // import { Radar } from 'react-chartjs-2';
@@ -258,61 +258,66 @@ export default MyChart;
 
 
 // // components/MyChart.js
-// import React from 'react';
-// import { Bar } from 'react-chartjs-2';
-// import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-// // Chart.js의 기본 구성 요소 등록
-// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+// Chart.js의 기본 구성 요소 등록
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-// const MyChart = () => {
-//   const data = {
-//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//     datasets: [
-//       {
-//         label: '첫번째 데이터셋',
-//         data: [65, 59, 33, 81, 56, 25, 40],
-//         backgroundColor: 'rgba(75, 75, 192, 0.2)',
-//         borderColor: 'rgba(75, 75, 192, 1)',
-//         borderWidth: 1,
-//       },
-//       {
-//         label: '두번째 데이터셋',
-//         data: [65, 50, 80, 8, 56, 55, 14],
-//         backgroundColor: 'rgba(85, 192, 192, 0.2)',
-//         borderColor: 'rgba(85, 192, 192, 1)',
-//         borderWidth: 1,
-//       },
-//     ],
-//   };
+const MyChart = () => {
+  const data = {
+    labels: ['Yes', ' No', 'Not Sure'],
+    datasets: [
+      {
+        label: 'Poll',
+        data: [6, 3, 9],
+        backgroundColor: (context) => {
+            const chart = context.chart;
+            const { ctx, chartArea } = chart;
+            // 차트가 없는 부분 필터링
+            if(!chartArea) {
+                return null;
+            }
+            return getGradient(chart);
+        },
+        borderWidth: 0,
+      },
+    ],
+  };
 
-//   const options = {
-//     // true로 설정하면 차트가 부모 요소의 크기에 따라 자동으로 크기를 조정
-//     responsive: true,
-//     plugins: {
-//         // legend 는 범례. 'top', 'bottom', 'left', 'right'
-//       legend: {
-//         position: 'bottom',
-//         labels: {
-//             font: {
-//               size: 20, // 범례의 글꼴 크기 (픽셀 단위)
-//             },
-//           },
-//       },
-//       title: {
-//         display: true,
-//         text: '차트 테스트',
-//         font: {
-//             size: 35, // 제목의 글꼴 크기 (픽셀 단위)
-//           },
-//       },
-//     },
-//   };
+  function getGradient(chart) {
+    const { ctx, chartArea: {top, bottom, left, right}, scales: {x, y} } = chart;
+    // top, bottom 순서는 내가 원하는대로 설정 가능. 지금은 아래에서 위쪽으로 그라데이션 효과
+    const gradientSegment = ctx.createLinearGradient(0, bottom, 0, top);
+    // 경계선 값 설정
+    let border = (bottom - y.getPixelForValue(3)) / (bottom - top);
+    console.log('border = ', border);
 
-//   return <Bar data={data} options={options} />;
-// };
+    if(border > 1) {
+        border = 1;
+    }
 
-// export default MyChart;
+    gradientSegment.addColorStop(0, 'red');
+    gradientSegment.addColorStop(border, 'orange');
+    gradientSegment.addColorStop(border, 'green');
+    gradientSegment.addColorStop(1, 'blue');
+    return gradientSegment;
+
+  }
+
+  const options = {
+   
+  };
+
+  return (
+    <div className='w-2/3'>
+        <Bar data={data} options={options} />;
+    </div>
+  )
+};
+
+export default MyChart;
 
 
 
