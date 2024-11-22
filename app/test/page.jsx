aa692c6ea72f6eb5f578a702c6ab8ab48d93c6b4
SCHcoding <codingb007@gmail.com>
2024-11-22 16:03:09 +0900

    Test: RadialBar Chart

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,5 +1,77 @@
 "use client"
 
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Chart.js 플러그인 등록
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const RadialBarChart = () => {
+  // 데이터 설정
+  const data = {
+    labels: ["Red", "Blue", "Green"], // 각각의 원에 대한 라벨
+    datasets: [
+      {
+        label: "Red",
+        data: [9], // [가치, 나머지 공간]
+        backgroundColor: ["#ff6384"], // 채우기 색상
+        borderWidth: 1, // 테두리 두께 제거
+        borderColor: "white",
+        cutout: "85%", // 내부 반지름 비율
+        // rotation: -90, // 시작 각도
+        circumference: 300, // 차트가 그려질 각도
+      },
+      {
+        label: "Blue",
+        data: [50],
+        backgroundColor: ["#36a2eb"],
+        borderWidth: 1,
+        borderColor: "white",
+        cutout: "85%",
+        circumference: 180,
+      },
+      {
+        label: "Green",
+        data: [80, 10],
+        backgroundColor: ["green", "black"],
+        borderWidth: 1,
+        borderColor: "white",
+        cutout: "85%",
+        circumference: 260,
+        // rotation: 270,
+      },
+    ],
+  };
+
+  // 옵션 설정
+  const options = {
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => `${context.label}: ${context.raw}`,
+        },
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  return (
+    <div style={{ width: "300px", height: "300px" }}>
+      <Doughnut data={data} options={options} />
+    </div>
+  );
+};
+
+export default RadialBarChart;
+
+
 // import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js';
 // import { Doughnut } from 'react-chartjs-2';
 
@@ -28,8 +100,6 @@
 //                     }
 //                 },
 //                 borderColor: ['purple', 'blue'],
-//                 circumference: 180,
-//                 rotation: 270,
 //             }]
 //     }
 
@@ -258,66 +328,56 @@
 
 
 // // components/MyChart.js
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+// import React from 'react';
+// import { Bar } from 'react-chartjs-2';
+// import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-// Chart.js의 기본 구성 요소 등록
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+// // Chart.js의 기본 구성 요소 등록
+// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const MyChart = () => {
-  const data = {
-    labels: ['Yes', ' No', 'Not Sure'],
-    datasets: [
-      {
-        label: 'Poll',
-        data: [6, 3, 9],
-        backgroundColor: (context) => {
-            const chart = context.chart;
-            const { ctx, chartArea } = chart;
-            // 차트가 없는 부분 필터링
-            if(!chartArea) {
-                return null;
-            }
-            return getGradient(chart);
-        },
-        borderWidth: 0,
-      },
-    ],
-  };
+// const MyChart = () => {
+//   const data = {
+//     labels: ['Yes', ' No', 'Not Sure'],
+//     datasets: [
+//       {
+//         label: 'Poll',
+//         data: [6, 3, 9],
+//         backgroundColor: "blue",
+//         borderWidth: 0,
+//       },
+//     ],
+//   };
 
-  function getGradient(chart) {
-    const { ctx, chartArea: {top, bottom, left, right}, scales: {x, y} } = chart;
-    // top, bottom 순서는 내가 원하는대로 설정 가능. 지금은 아래에서 위쪽으로 그라데이션 효과
-    const gradientSegment = ctx.createLinearGradient(0, bottom, 0, top);
-    // 경계선 값 설정
-    let border = (bottom - y.getPixelForValue(3)) / (bottom - top);
-    console.log('border = ', border);
+//   const barPattern = {
+//     id: `barPattern`,
+//     beforeDatasetsDraw(chart, args, pluginOptions) {
+//         const { ctx, chartArea: { top, bottom, height }, scales: {x, y} } = chart;
 
-    if(border > 1) {
-        border = 1;
-    }
+//         ctx.save();
+//         const width = chart.getDatasetMeta(0).data[0].width;
+//         console.log("width = ", width); // 지금 기본 그래프 width 구하기 
 
-    gradientSegment.addColorStop(0, 'red');
-    gradientSegment.addColorStop(border, 'orange');
-    gradientSegment.addColorStop(border, 'green');
-    gradientSegment.addColorStop(1, 'blue');
-    return gradientSegment;
+//         chart.getDatasetMeta(0).data.forEach((bar, index) => {
+//             ctx.fillRect(x.getPixelForValue(index) - width / 2, top, width, height - 0.5);
+//         });
 
-  }
+//       }
+//     }
 
-  const options = {
+//   const options = {
    
-  };
+//   };
 
-  return (
-    <div className='w-2/3'>
-        <Bar data={data} options={options} />;
-    </div>
-  )
-};
 
-export default MyChart;
+
+//   return (
+//     <div className='w-2/3'>
+//         <Bar data={data} options={options} plugins={[barPattern]} />;
+//     </div>
+//   )
+// };
+
+// export default MyChart;
 
 
 
